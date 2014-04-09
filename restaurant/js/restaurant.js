ce10e37f7253d251d7dba6ec74e1973ff8dc02f4
Pedro Vitti <pedrovitti@secplus.com.br>
2014-04-09 18:58:36 -0300

    First release of restaurant app

--- restaurant/js/restaurant.js
+++ restaurant/js/restaurant.js
@@ -1,4 +1,10 @@
 function RestaurantController($scope) {
+    $scope.TableDoesntExistsError = function () {
+        return {
+            msg: "This table does not exists."
+        };
+    };
+    
     $scope.searchString = "";
     $scope.name = "Restaurante";
     $scope.menu = {
@@ -25,29 +31,29 @@ function RestaurantController($scope) {
         "1": [
             {
                 "id": "mussarela",
-                "quantity": "2"
+                "quantity": 2
             },
             {
                 "id": "mussarela",
-                "quantity": "2"
+                "quantity": 2
             },
             {
                 "id": "mussarela",
-                "quantity": "2"
+                "quantity": 2
             }
         ],
         "2": [
             {
                 "id": "camarao",
-                "quantity": "1"
+                "quantity": 1
             },
             {
                 "id": "marguerita",
-                "quantity": "1"
+                "quantity": 1
             },
             {
                 "id": "camarao",
-                "quantity": "1"
+                "quantity": 1
             }
         ]
     };
@@ -68,5 +74,41 @@ function RestaurantController($scope) {
     $scope.remove = function (itemIndex) {
         //$scope.menu.splice(contactIndex, 1);
         $scope.item = {};
+    };
+    
+    $scope.addMenu = function (menuparam) {
+        $scope.menu = menuparam;
+    };
+
+    $scope.order = function (mesa, pedido) {
+        if (!$scope.orders[mesa]) {
+            $scope.orders[mesa] = [];
+        }
+        $scope.orders[mesa].push(pedido);
+    };
+
+    $scope.total = function (table) {
+        var total = 0,
+            itemNumber = 0;
+
+        if (!$scope.orders[table]) { throw new $scope.TableDoesntExistsError(); }
+        for (itemNumber = 0; itemNumber < $scope.orders[table].length; itemNumber += 1) {
+            var orderItem = $scope.orders[table][itemNumber];
+            var menuItem = $scope.menu[orderItem.id];
+            total += menuItem.price * orderItem.quantity;       
+        }
+        console.log(total);
+        return total;
+    };
+
+    $scope.close = function (table) {
+        if (!$scope.orders[table]) { throw new $scope.TableDoesntExistsError(); }
+        var closeTotal = $scope.total(table);
+        delete $scope.orders[table];
+        return closeTotal;
+    };
+
+    $scope.find = function (item) {
+        return $scope.menu[item] || {};
     };
 }
