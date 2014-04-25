14ce1bc499ca229285e0acb7a28f8e6a19b19447
Pedro Vitti <pedrovitti@secplus.com.br>
2014-04-25 15:59:59 -0300

    Add static root dir to restaurant server

--- restaurant/js/restaurant-server.js
+++ restaurant/js/restaurant-server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var body_parser = require('body-parser');
+var path = require('path');
 var app = express();
 
 app.use(body_parser());
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.listen(3412);
